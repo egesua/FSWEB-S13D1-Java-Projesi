f5c530fb19df6224f76888b537fe9fd5c2a89643
egesua <egesu06@gmail.com>
2023-11-13 21:23:06 +0300

    fixed the israeli pakestani conflict

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,42 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        boolean isSummer = true;
-        int temp1 = 30;
-        int temp2 = 40;
-        boolean bark = true;
-        int hour = 10;
+        System.out.println(shouldWakeUp(true, 9));
+        System.out.println(hasTeen(9,99,19));
+        System.out.println(isCatPlaying(true, 40));
 
-        boolean result = shouldWakeUp(bark, hour);
-        System.out.println("Result is: " + result);
-
-        boolean result2 = hasTeen(9, 99, 19);
-        System.out.println(result2);
-
-        boolean result3 = isCatPlaying(isSummer, temp1);
-        boolean result4 = isCatPlaying(false, temp2);
-        System.out.println(result3);
-        System.out.println(result4);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter width of a rectangle: ");
+        double width = scanner.nextDouble();
+        System.out.println("Enter height of a rectangle: ");
+        double height = scanner.nextDouble();
+        System.out.println("Area of the rectangle: " + area(height, width));
     }
 
-    public static boolean shouldWakeUp(boolean bark, int hour) {
-        return bark && (hour < 8 || hour >= 20);
+    public static boolean shouldWakeUp(boolean isBark, int hour) {
+        if (hour < 0 || hour > 23) return false;
+        if (!isBark) return false;
+        return hour < 8 || hour >= 20;
     }
 
-    public static boolean hasTeen(int age1, int age2, int age3) {
-        return isTeen(age1) || isTeen(age2) || isTeen(age3);
-    }
-
-    public static boolean isTeen(int age) {
-        if (age < 13 || age > 19) {
-            return false;
-        } else {
-            return true;
+    public static boolean hasTeen(int... ages) {
+        for(int age: ages){
+            if(age >= 13 && age <= 19) return true;
         }
+        return false;
     }
 
-    public static boolean isCatPlaying(boolean isSummer, int temp) {
-        int maxDegree = isSummer ? 45 : 35;
-        return temp >= 25 && temp <= maxDegree;
+    public static boolean isCatPlaying(boolean isSummer, int temperature) {
+        if (isSummer) {
+            return temperature > 25 && temperature < 35;
+        }
+        return temperature > 25 && temperature < 45;
+    }
+
+    public static double area(double height, double width) {
+        if(width < 0 || height < 0) return -1;
+        return width * height;
     }
 }
+
